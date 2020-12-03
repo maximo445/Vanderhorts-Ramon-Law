78a65b993d7aa6be32f3ac16d353a162f15fda35
Maximo Brito <maximobrito@Maximos-Mini.attlocal.net>
2020-12-02 20:40:41 -0500

    Project Started

---
+++ app.js
@@ -0,0 +1,12 @@
+
+// const homeH3 = document.querySelector('.home');
+// const whatWeDoH3 = document.querySelector('.whatWeDo');
+// const blogH3 = document.querySelector('.blog');
+
+// const homeLine = document.querySelector('.homeLine');
+// const doLine = document.querySelector('.doLine');
+// const blogLine = document.querySelector('.blogLine');
+
+// homeLine.style.maxWidth = homeH3.style.width;
+// doLine.style.maxWidth = whatWeDoH3.style.width;
+// blogH3.style.maxWidth = blogH3.style.width;
